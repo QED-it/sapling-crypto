527a70b3bcec698e7c71bfccf41e3e0517de867c
Constance Beguier <constance@qed-it.com>
2025-09-08 12:06:35 +0200

    Remove TODO comment

--- src/bundle.rs
+++ src/bundle.rs
@@ -316,7 +316,7 @@ impl SpendDescriptionV5 {
         self,
         anchor: bls12_381::Scalar,
         zkproof: GrothProofBytes,
-        spend_auth_sig: redjubjub::Signature<SpendAuth>, // TODO
+        spend_auth_sig: redjubjub::Signature<SpendAuth>,
     ) -> SpendDescription<A>
     where
         A: Authorization<SpendProof = GrothProofBytes, AuthSig = redjubjub::Signature<SpendAuth>>,
